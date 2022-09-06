8e90e6903e66bead4dcc398d5a8c396d4a1b3d39
SimLukos <simonas.lukosevicius92@gmail.com>
2022-09-06 23:45:48 +0300

    Task 5 completed

--- Task 5/script.js
+++ Task 5/script.js
@@ -9,13 +9,43 @@ atveju grąžins masyvą su "users", kurie yra pilnamečiai.
 -------------------------------------------------------------------------- */
 
 const users = [
-  { id: '1', name: 'John Smith', age: 20, hasDog: true },
-  { id: '2', name: 'Ann Smith', age: 24, hasDog: false },
-  { id: '3', name: 'Tom Jones', age: 31, hasDog: true },
-  { id: '4', name: 'Rose Peterson', age: 17, hasDog: false },
-  { id: '5', name: 'Alex John', age: 25, hasDog: true },
-  { id: '6', name: 'Ronald Jones', age: 63, hasDog: true },
-  { id: '7', name: 'Elton Smith', age: 16, hasDog: true },
-  { id: '8', name: 'Simon Peterson', age: 30, hasDog: false },
-  { id: '9', name: 'Daniel Cane', age: 51, hasDog: true },
+  { id: "1", name: "John Smith", age: 20, hasDog: true },
+  { id: "2", name: "Ann Smith", age: 24, hasDog: false },
+  { id: "3", name: "Tom Jones", age: 31, hasDog: true },
+  { id: "4", name: "Rose Peterson", age: 17, hasDog: false },
+  { id: "5", name: "Alex John", age: 25, hasDog: true },
+  { id: "6", name: "Ronald Jones", age: 63, hasDog: true },
+  { id: "7", name: "Elton Smith", age: 16, hasDog: true },
+  { id: "8", name: "Simon Peterson", age: 30, hasDog: false },
+  { id: "9", name: "Daniel Cane", age: 51, hasDog: true },
 ];
+
+// creating empty arrays
+const usersHaveDogs = [];
+const grownUps = [];
+
+// ---------------
+function filterDogOwners(data) {
+  data.forEach((element) => {
+    if (element.hasDog === true) {
+      usersHaveDogs.push(element);
+    } else {
+      console.log("Neturi: " + element);
+    }
+  });
+  console.log(usersHaveDogs);
+}
+
+filterDogOwners(users);
+
+// ---------------
+function filterAdults(data) {
+  data.forEach((element) => {
+    if (element.age >= 18) {
+      grownUps.push(element);
+    }
+  });
+  console.log(grownUps);
+}
+
+filterAdults(users);
