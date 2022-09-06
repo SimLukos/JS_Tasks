f765f49fb0ecc16fbed66a9fb54003be3e761611
SimLukos <simonas.lukosevicius92@gmail.com>
2022-09-07 01:29:54 +0300

    Task 9 completed

--- Task 9/script.js
+++ Task 9/script.js
@@ -9,3 +9,17 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+class Movie {
+  constructor(title, director, budget) {
+    this.title = title;
+    this.director = director;
+    this.budget = budget;
+  }
+  wasExpensive() {
+    if (this.budget >= 100000000) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
