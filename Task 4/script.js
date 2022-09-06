4fa8c5a2b64bc8c8b534e8846ae0d46145db40d9
SimLukos <simonas.lukosevicius92@gmail.com>
2022-09-06 23:27:09 +0300

    Task 4 completed

--- Task 4/script.js
+++ Task 4/script.js
@@ -9,4 +9,36 @@ Pastaba: Sukurta kortelė, kurioje yra informacija apie automobilį (brand), tur
 turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'cars.json';
+const ENDPOINT = "cars.json";
+
+function getData() {
+  fetch(ENDPOINT, { method: "GET" })
+    .then((responsive) => responsive.json())
+    .then((data) => {
+      data.forEach((element) => {
+        // creating elements
+        const brandContainer = document.createElement("div");
+        brandContainer.className = "brandContainer";
+        const brandName = document.createElement("h3");
+        brandName.className = "brandName";
+        const list = document.createElement("ul");
+
+        element.models.forEach((section) => {
+          const model = document.createElement("li");
+          model.textContent = section;
+
+          //   adding to list
+          list.append(model);
+        });
+
+        brandName.textContent = element.brand;
+
+        // adding brand name and model list to container
+        brandContainer.append(brandName, list);
+
+        document.getElementById("output").append(brandContainer);
+      });
+    });
+}
+
+getData();
