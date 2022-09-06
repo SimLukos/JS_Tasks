899be0561056dfe18c9dc6c10a2b61a95ed2346e
SimLukos <simonas.lukosevicius92@gmail.com>
2022-09-07 00:00:18 +0300

    Task 6 completed

--- Task 6/script.js
+++ Task 6/script.js
@@ -3,19 +3,46 @@ Turimas "users" masyvas.
 
 Parašykite funckijas, kurios atlikas nurodytas užduotis:
 1. funkcija "getUserAverageAge" - kaip argumentą priims masyvą ir duoto masyvo 
-atveju grąžins visų "users" amžiaus visurkį kaip skaičių.
+atveju grąžins visų "users" amžiaus vidurkį kaip skaičių.
 2. funkcija "getUsersNames" -  kaip argumentą priims masyvą ir duoto masyvo 
 atveju grąžins visų "users" vardus naujame masyve pvz., ['John Smith', 'Ann Smith'..].
 -------------------------------------------------------------------------- */
 
 const users = [
-  { id: '1', name: 'John Smith', age: 20 },
-  { id: '2', name: 'Ann Smith', age: 24 },
-  { id: '3', name: 'Tom Jones', age: 31 },
-  { id: '4', name: 'Rose Peterson', age: 17 },
-  { id: '5', name: 'Alex John', age: 25 },
-  { id: '6', name: 'Ronald Jones', age: 63 },
-  { id: '7', name: 'Elton Smith', age: 16 },
-  { id: '8', name: 'Simon Peterson', age: 30 },
-  { id: '9', name: 'Daniel Cane', age: 51 },
+  { id: "1", name: "John Smith", age: 20 },
+  { id: "2", name: "Ann Smith", age: 24 },
+  { id: "3", name: "Tom Jones", age: 31 },
+  { id: "4", name: "Rose Peterson", age: 17 },
+  { id: "5", name: "Alex John", age: 25 },
+  { id: "6", name: "Ronald Jones", age: 63 },
+  { id: "7", name: "Elton Smith", age: 16 },
+  { id: "8", name: "Simon Peterson", age: 30 },
+  { id: "9", name: "Daniel Cane", age: 51 },
 ];
+
+// -----------------
+function getUserAverageAge(data) {
+  let suma = 0;
+  let number = 0;
+  data.forEach((element) => {
+    suma = suma + element.age;
+    ++number;
+  });
+
+  const averageAge = suma / number;
+  console.log("Amziaus vidurkis: " + averageAge);
+}
+
+getUserAverageAge(users);
+
+// -----------------
+
+function getUsersNames(data) {
+  const onlyNames = [];
+  data.forEach((element) => {
+    onlyNames.push(element.name);
+  });
+  console.log(onlyNames);
+}
+
+getUsersNames(users);
