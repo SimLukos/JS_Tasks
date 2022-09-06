09cb5bd081f4d1d1e153ca22a9ec990ec27de397
SimLukos <simonas.lukosevicius92@gmail.com>
2022-09-06 18:25:20 +0300

    Adding existing files to GitHub

---
+++ Task 4/script.js
@@ -0,0 +1,12 @@
+/* ------------------------------ TASK 4 -----------------------------------
+Parašykite JS kodą, vartotojui atėjus į tinklapį kreipsis į cars.json failą
+ir iš atvaizduos visus automobilių gamintojus ir pagamintus modelius. 
+Kiekvienas gamintojas turės savo atvaizdavimo "kortelę", kurioje bus 
+nurodomas gamintojas ir jo pagaminti modeliai.
+
+
+Pastaba: Sukurta kortelė, kurioje yra informacija apie automobilį (brand), turi 
+turėti bent minimalų stilių ir būti responsive;
+-------------------------------------------------------------------------- */
+
+const ENDPOINT = 'cars.json';
